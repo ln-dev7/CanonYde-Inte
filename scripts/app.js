2ca4fa299cf0b43fb65337f15c10b66ae16aa5bc
ln-dev7 <leonelngoya@gmail.com>
2022-07-20 22:41:02 +0100

    responsive du menu

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+const menuClose = document.querySelector(".menu-close");
+const menuOpen = document.querySelector(".header-left-menu");
+const menu = document.querySelector(".menu");
+
+menuOpen.addEventListener("click", () => {
+  menu.classList.add("menu-open");
+});
+menuClose.addEventListener("click", () => {
+  menu.classList.remove("menu-open");
+});
