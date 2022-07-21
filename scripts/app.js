9989f7a9f042daca7d8168d688ceb363c4df42d3
ln-dev7 <leonelngoya@gmail.com>
2022-07-21 07:35:28 +0100

    Ajout du loader

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,9 @@
 // ----------------------------------------------------------------------------
+const loader = document.querySelector("#loader");
+window.addEventListener("load", () => {
+  loader.classList.add("loaded");
+});
+// ----------------------------------------------------------------------------
 const menuClose = document.querySelector(".menu-close");
 const menuOpen = document.querySelector(".header-left-menu");
 const menu = document.querySelector(".menu");
